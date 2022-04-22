5a79f09b7ddff17e9f5f8d60d76114e018945675
bambanggunawanid <bambanggunawan887@gmail.com>
2022-04-22 22:45:58 +0700

    final code amr oclone

--- path_planning/unit2_pp/scripts/unit2_solution_server.py
+++ path_planning/unit2_pp/scripts/unit2_solution_server.py
@@ -15,10 +15,10 @@ from gridviz import GridViz
 from unit2_solution import dijkstra
 
 def make_plan(req):
-  ''' 
+  '''
   Callback function used by the service server to process
   requests from clients. It returns a msg of type PathPlanningPluginResponse
-  ''' 
+  '''
   # costmap as 1-D array representation
   costmap = req.costmap_ros
   # number of columns in the occupancy grid
@@ -63,7 +63,7 @@ def clean_shutdown():
 if __name__ == '__main__':
   rospy.init_node('dijkstra_path_planning_service_server', log_level=rospy.INFO, anonymous=False)
   make_plan_service = rospy.Service("/move_base/SrvClientPlugin/make_plan", PathPlanningPlugin, make_plan)
-  cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+  cmd_vel = rospy.Publisher('/hoverboard_velocity_controller/cmd_vel', Twist, queue_size=5)
   rospy.on_shutdown(clean_shutdown)
 
   while not rospy.core.is_shutdown():
